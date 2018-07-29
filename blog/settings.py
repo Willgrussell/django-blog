66f31eb49acf55e3672ccbdb58ed4298d12e0c81
William Russell <russellgwill@gmail.com>
2018-07-29 19:43:07 +0000

    Add media, posts, static, templates with amendments to .py files

--- blog/settings.py
+++ blog/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = '323ks#*62hr2^+-7xy$d=+y)zzrvem3yh=!=98f4_=_phg6=7i'
 DEBUG = True
 
 ALLOWED_HOSTS = [
+        '127.0.0.1', os.environ.get('C9_HOSTNAME'),
         'django-blog-willru55ell.c9users.io'
     ]
-
 
 # Application definition
 
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_forms_bootstrap',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -64,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -120,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
